1acb22b15d53a80e15785bcb674327512f9fc2e6
Nat Ritmeyer <nat@natontesting.com>
2016-06-02 19:49:40 +0100

    adding shared driver to pico

--- src/test/java/automation/PicoDependencyInjector.java
+++ src/test/java/automation/PicoDependencyInjector.java
@@ -1,9 +1,11 @@
 package automation;
 
+import automation.ui.SharedDriver;
 import cucumber.runtime.java.picocontainer.PicoFactory;
 
 public class PicoDependencyInjector extends PicoFactory {
 
     public PicoDependencyInjector() {
+        addClass(SharedDriver.class);
     }
 }
